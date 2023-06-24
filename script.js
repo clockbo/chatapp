5925d4cc32ad07c4bd2264a2ef26388262a23f8a
baba <sagar.ktechgmail.com>
2023-06-24 09:57:49 +0530

    online

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000/');
+const socket = io('https://simple-chat-application-2hvv.onrender.com/');
 
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
