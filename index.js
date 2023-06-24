b308ccac7a32fe369218c70b05b1d9fa0f926e67
baba <sagar.ktechgmail.com>
2023-06-24 09:15:32 +0530

    complete the chat app

--- index.js
+++ index.js
@@ -12,4 +12,13 @@ io.on('connection', socket => {
         users[socket.id] = name;
         socket.broadcast.emit('user-connected', name)
     })
+
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id] })
+    })
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id]
+    })
 })
--- script.js
+++ script.js
@@ -6,5 +6,31 @@ const messageInput = document.getElementById('message-input');
 
 let name = prompt('What is your name');
 console.log(name)
+appendMessage('You Joined')
 
 socket.emit('new-user', name)
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name} : ${data.message}`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    appendMessage(`You : ${message}`)
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
+})
+
+function appendMessage(message){
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement)
+}
